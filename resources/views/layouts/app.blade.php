a65e7ac81cc212e4c7fe113b69cdc0d6f202889c
strife.jave <j.strf0124@gmail.com>
2020-07-04 06:48:35 +0000

    add login method

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     </head>
-
     <body>
-
         @include('commons.navbar')
-        
         <div class="container">
             @include('commons.error_messages')
             
